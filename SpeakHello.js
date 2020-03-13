59aa114bc1dd5b099c661bc5371e8a6bd69d435c
efrem <61226437+efremtecle29@users.noreply.github.com>
2020-03-13 16:12:27 +0300

    SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,9 +1,15 @@
-(function(window) { 
-var helloSpeaker = {};
+(function(window){
+
+
+
+var helloSpeaker ={};
 var speakWord = "Hello";
-helloSpeaker.speak = function(name) {
+
+
+helloSpeaker.speak=function(name) {
   console.log(speakWord + " " + name);
 }
-window.helloSpeaker = helloSpeaker;
-}
-)(window);
+
+
+
+window.helloSpeaker = helloSpeaker;})(window);
