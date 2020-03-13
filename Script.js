13a19fc2c228fd860b92440357458719646b6585
efrem <61226437+efremtecle29@users.noreply.github.com>
2020-03-13 16:13:36 +0300

    Script.js

--- Script.js
+++ Script.js
@@ -1,14 +1,20 @@
-(function () {
+
+
+(function() {
+
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
- 
+
+  
   var firstLetter = names[i].charAt(0).toLowerCase();
 
-    if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+ currentName=names[i];
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(currentName);
+
   } else {
-    helloSpeaker.speak(names[i]);
+    helloSpeaker.speak(currentName);
   }
 }
 
